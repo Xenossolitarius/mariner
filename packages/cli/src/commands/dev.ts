45aa717ddf32392c5ec09a8dbb0a411746e42ae6
Xenossolitarius <ivan.t.pesic@gmail.com>
2024-01-24 19:18:38 +0100

    Created Koa middleware server

--- packages/cli/src/commands/dev.ts
+++ packages/cli/src/commands/dev.ts
@@ -1,8 +1,11 @@
-import { loadConfigFromFile, createServer } from 'vite'
+import { loadConfigFromFile, createServer as createViteServer } from 'vite'
 import { program } from '@commander-js/extra-typings'
 import { options as serverOptions } from './shared/server'
 import { STARTING_DEV_SERVER } from '../messages'
 import { configure } from '../steps/configure'
+
+import Koa from 'koa'
+import koaConnect from 'koa-connect'
 
 serverOptions(program.command('dev', { isDefault: true }).description('Serve Micro Frontends'))
   .option('-m, --mode <mode>', 'Specify the mariner mode')
@@ -21,10 +24,23 @@ serverOptions(program.command('dev', { isDefault: true }).description('Serve Mic
 
     console.log(file.config)
 
-    const server = await createServer({ ...file.config, root: projects[0].root, configFile: false })
+    const viteServer = await createViteServer({ ...file.config, root: projects[0].root, configFile: false , server: {middlewareMode: true}, appType: 'custom'})
 
-    console.log(server.config)
+    const app = new Koa()
 
-    await server.listen(3000)
-    server.printUrls()
+    app.use(koaConnect(viteServer.middlewares))
+
+
+    app.listen(3000, () => {
+      console.log('Started')
+    })
+
+
+
+    // const server = await createServer({ ...file.config, root: projects[0].root, configFile: false })
+
+    // console.log(server.config)
+
+    // await server.listen(3000)
+    // server.printUrls()
   })
