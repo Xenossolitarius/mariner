c1149d3b0f40b27dce937a7e990c330f3f8059aa
Xenossolitarius <ivan.t.pesic@gmail.com>
2024-01-29 09:17:50 +0100

    Fix react playground

--- playground/app3/navigator.ts
+++ playground/app3/navigator.ts
@@ -2,4 +2,4 @@ import { defineNavigator, createReactNavigator } from 'mariner/navigator';
 import { NavigatorApp } from './src/main-navigator'
 import ReactDOM from 'react-dom/client'
 
-export default defineNavigator({ navigator: createReactNavigator(ReactDOM.createRoot,NavigatorApp)})
+export default defineNavigator({ navigator: createReactNavigator(ReactDOM.createRoot,NavigatorApp())})
