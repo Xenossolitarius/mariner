8e6c9cd466ef1bf1728556d4602fcdeda048edb5
Ivan Pešić <ivan.pesic@spaceinch.com>
2026-03-22 01:04:12 +0100

    Generate mariner files

--- playground/.mariner/mariner.d.ts
+++ playground/.mariner/mariner.d.ts
@@ -1,55 +1,60 @@
 declare module 'navigator:app1' {
-  import { Navigator as Navigator_2 } from 'mariner-fe/navigator'
 
-  const navigator_2: Navigator_2
+  import { Navigator as Navigator_2 } from 'mariner-fe/navigator';
+  
+  const navigator_2: Navigator_2;
   export { navigator_2 as navigator }
-
-  export {}
+  
+  export { }
+  
 }
 declare module 'navigator:app2' {
-  import { Navigator as Navigator_2 } from 'mariner-fe/navigator'
 
-  const navigator_2: Navigator_2
+  import { Navigator as Navigator_2 } from 'mariner-fe/navigator';
+  
+  const navigator_2: Navigator_2;
   export { navigator_2 as navigator }
-
-  export {}
+  
+  export { }
+  
 }
 declare module 'navigator:app3' {
-  import { Navigator as Navigator_2 } from 'mariner-fe/navigator'
 
-  const navigator_2: Navigator_2
+  import { Navigator as Navigator_2 } from 'mariner-fe/navigator';
+  
+  const navigator_2: Navigator_2;
   export { navigator_2 as navigator }
-
-  export {}
+  
+  export { }
+  
 }
 declare module 'navigator:envs' {
-  export const MARINER_GLOBAL_VARIABLE: any
 
-  export {}
+  export const MARINER_GLOBAL_VARIABLE: any;
+  
+  export { }
+  
 }
 declare module 'navigator:lazy' {
-  export const lazy: () => void
 
-  export {}
+  export const lazy: () => void;
+  
+  export { }
+  
 }
 declare module 'navigator:shared' {
-  import { Pinia } from 'pinia'
-  import { StoreDefinition } from 'pinia'
 
-  export const pinia: Pinia
-
-  export const useCounter: StoreDefinition<
-    'counter',
-    {
-      counter: number
-    },
-    {},
-    {
-      update(): void
-    }
-  >
-
-  export * from 'pinia'
-
-  export {}
+  import { Pinia } from 'pinia';
+  import { StoreDefinition } from 'pinia';
+  
+  export const pinia: Pinia;
+  
+  export const useCounter: StoreDefinition<"counter", {
+  counter: number;
+  }, {}, {
+  update(): void;
+  }>;
+  
+  export { }
+  
 }
